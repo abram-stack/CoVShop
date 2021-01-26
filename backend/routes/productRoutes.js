284bcfcdf99533df50cfcf091e82b6a41bc0d20a
abram-stack <anthonybramantyo@outlook.com>
2021-01-26 18:18:42 +0100

    create custom errorHandler, middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -23,10 +23,11 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res ) => {
   const product = await Product.findById(req.params.id);
 
-  if(!product) {
-    res.status(404).json({message: 'product not found'});
-  }else{
+  if(product) {
     res.json(product);
+  }else{
+   res.status(404);
+   throw new Error('Product not found');
   }
 
 }));
