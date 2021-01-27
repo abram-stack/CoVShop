8900b9af81842dec1b14ab9d465879ad9de88df2
abram-stack <anthonybramantyo@outlook.com>
2021-01-27 18:28:22 +0100

    loader, message component

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,7 +11,6 @@ const router = express.Router();
 // @access  Public
 router.get('/', asyncHandler(async (req, res) => {
   const products = await Product.find({});
-
   res.json(products);
 }));
 
