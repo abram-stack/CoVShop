800fb9e3db9d5d6123cc1bc0610e63c007d59409
abram-stack <anthonybramantyo@outlook.com>
2021-01-27 22:51:11 +0100

    add qty, component level state

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
@@ -16,7 +16,7 @@ const ProductScreen = ({ match }) => {
   // we will need props.match or using destructure so we'll use only match
   // const product = products.find(p => p._id === match.params.id )
   
-
+  const [ qty, setQty ] = useState(0);  
   const dispatch = useDispatch();
 
   const productDetails = useSelector(state => state.productDetails)
@@ -74,6 +74,23 @@ const ProductScreen = ({ match }) => {
                   </Col>
                 </Row>
               </ListGroup.Item>
+
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Quantity</Col>
+                    <Col>
+                      <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
+                        {/* whatever the quantity in db */}
+                        {/* ie stock = 5 [0,1,2,4] */}
+                        {[...Array(product.countInStock).keys()].map( x => (
+                          <option key={x + 1} value={x + 1}> {x + 1 } </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
 
               <ListGroup.Item>
                 <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
